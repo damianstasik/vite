6667305d549471df704586694240493adc1cea35
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2024-06-11 22:04:06 +0200

    chore(types): add comments to types of built-in import.meta.env variables

--- packages/vite/types/importMeta.d.ts
+++ packages/vite/types/importMeta.d.ts
@@ -4,10 +4,18 @@
 
 interface ImportMetaEnv {
   [key: string]: any
+  /** The base url the app is being served from. This is determined by the [`base` config option](https://vitejs.dev/config/shared-options#base). */
   BASE_URL: string
+  /**
+   * The [mode](https://vitejs.dev/guide/env-and-mode#modes) the app is running in.
+   * By default, the dev server (`dev` command) runs in `development` mode and the `build` command runs in `production` mode.
+   */
   MODE: string
+  /** Whether the app is running in development (always the opposite of `import.meta.env.PROD`). */
   DEV: boolean
+  /** Whether the app is running in production (running the dev server with `NODE_ENV='production'` or running an app built with `NODE_ENV='production'`). */
   PROD: boolean
+  /** Whether the app is running in the [server](https://vitejs.dev/guide/ssr#conditional-logic). */
   SSR: boolean
 }
 
